2e42b66eb07115952ce2cfeb01b1b9599dd3d39a
Omar Velazquez <ovelazquez@convergiq.com>
2015-04-12 12:56:25 -0500

    Plot 1 and 2 first try

--- plot1.R
+++ plot1.R
@@ -3,7 +3,11 @@
 ## using initialize.R script, this script is used to keep clean this plotting Script,
 ## please go to initialize.R script to check the common code used to download, unzip, create and subset the data frame
 source("initialize.R")
-## Plotting
-
 ## Sending to PNG file
+png(paste(getwd(),"/plot1.png",sep=""))
+## Plotting
+if(exists("dataFrameSubset")){
+    hist(dataFrameSubset$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",ylab="Frequency")
+}
+dev.off()
 
